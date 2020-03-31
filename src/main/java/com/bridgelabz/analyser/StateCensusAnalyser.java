daeba9f3093554d378f37c7932c81babbee4bd17
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-31 12:53:42 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/bridgelabz/analyser/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.analyser;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to India State Census Analyser");
+    }
+}
